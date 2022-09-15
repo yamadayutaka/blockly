e9aeb121429e96669e46acc478936c9a38541496
YAMADA Yutaka <yamadayutaka@sysj.co.jp>
2022-09-15 20:50:19 +0900

    build: build/test on windows
    * chore(deps): bump @hyperjump/json-schema from 0.18.4 to 0.18.5
    * chore(deps): add gulp-gzip 1.4.2
    * build: migrate test scripts to gulp task (test_tasks.js)
    * build: not to use the grep command
    * build: normalize path

---
+++ scripts/build_helper.js
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Gulp script to build Blockly for Node & NPM.
+ * Run this script by calling "npm install" in this directory.
+ */
+/* eslint-env node */
+
+const path = require('path');
+
+/**
+ * Escape regular expression pattern by making the following replacements:
+ *  * single backslash -> double backslash
+ * @param {string} pattern regular expression pattern
+ * @return {string} escaped regular expression pattern
+ */
+function escapePattern(pattern) {
+  return pattern.replace(/\\/g, '\\\\');
+}
+
+/**
+ * Replaces OS-specific path with POSIX style path.
+ * @param {string} target target path
+ * @return {string} normalized path
+ */
+function normalizePath(target) {
+  const osSpecificSep = new RegExp(escapePattern(path.sep), 'g');
+  return target.replace(osSpecificSep, path.posix.sep);
+}
+
+module.exports = {
+  normalizePath,
+};
